f2a4751e71a30d2e881e723d6c9ce45b76404dc3
ShoeiAzekawa <1206079@st.anan-nct.ac.jp>
2022-08-16 16:47:53 +0900

    receiveListのcssを大幅更新

--- scripts/getRequest.js
+++ scripts/getRequest.js
@@ -1,32 +1,45 @@
 let dataList = [
-    {rank:1,request_name:"êhÇ¢Ç‡ÇÃÇêHÇ◊ÇÈ",author:"azekawa"},
-    {rank:2,request_name:"ãÿì˜Çé©ñùÇµÇƒ",author:"okada"},
-    {rank:3,request_name:"Speak English!",author:"tom"},
+    {rank:1,request_name:"èbì˜êHÇ¡ÇƒÇ›ÇΩ",author:"azekawa",movie_count:3},
+    {rank:2,request_name:"èbì˜êHÇ¡ÇƒÇ›ÇΩÇQ",author:"okada",movie_count:2},
+    {rank:3,request_name:"Speak English!",author:"tom",movie_count:5},
 ];
 
 function getRequestList(){
     let infoHtml = ''
 
     for(const data of dataList){
-        infoHtml = infoHtml + '<button id="request" onclick="location.href=\'Recieve.html\'">';
-        infoHtml = infoHtml + '<img src="../images/';
-
-        if(data.rank == 0)infoHtml = infoHtml+'icon_rank_A.svg';
-        if(data.rank == 1)infoHtml = infoHtml+'icon_rank_B.svg';
-        if(data.rank == 2)infoHtml = infoHtml+'icon_rank_C.svg';
-        if(data.rank == 3)infoHtml = infoHtml+'icon_rank_D.svg';
-        if(data.rank == 4)infoHtml = infoHtml+'icon_rank_E.svg';
-
-        infoHtml = infoHtml + '" width= "80px"';
-        infoHtml = infoHtml + '>';
-
-        infoHtml = infoHtml + '<span="requestname">' + data.request_name + '</span>';
-        infoHtml = infoHtml + '<span="author">' + data.author + '</span>';
-        infoHtml = infoHtml + '</button><br>\n'
+        infoHtml = infoHtml + '<button class="button" id="request" onclick="location.href=\'Recieve.html\'">';
+            infoHtml = infoHtml + '<div class="whole">';
+                infoHtml = infoHtml + '<div class="above">';
+                    infoHtml = infoHtml + '<div class="left">';
+                        infoHtml = infoHtml + '<img class="rank" src="../images/';
+                        if(data.rank == 0)infoHtml = infoHtml + 'icon_rank_A.svg';
+                        if(data.rank == 1)infoHtml = infoHtml + 'icon_rank_B.svg';
+                        if(data.rank == 2)infoHtml = infoHtml + 'icon_rank_C.svg';
+                        if(data.rank == 3)infoHtml = infoHtml + 'icon_rank_D.svg';
+                        if(data.rank == 4)infoHtml = infoHtml + 'icon_rank_E.svg';
+                        infoHtml = infoHtml + '" width= "50px"';
+                        infoHtml = infoHtml + '>';
+                    infoHtml = infoHtml + '</div>';
+                    infoHtml = infoHtml + '<div class="right">';
+                        infoHtml = infoHtml + '<div class="request-name">' + data.request_name + '</div>';
+                        infoHtml = infoHtml + '<div class="user-name">' + data.author + '</div>';
+                        infoHtml = infoHtml + '<div class="ants">';
+                            for(let i = 1; i <= data.movie_count; i++) {
+                                infoHtml = infoHtml + '<div class="ant"></div>';
+                            }
+                        infoHtml = infoHtml + '</div>';
+                    infoHtml = infoHtml + '</div>';
+                infoHtml = infoHtml + '</div>';
+                infoHtml = infoHtml + '<div class="below">';
+                    infoHtml = infoHtml + '<img class="picture" src="../images/kemono.jpg" alt="picture" height="150px">';
+                infoHtml = infoHtml + '</div>';
+            infoHtml = infoHtml + '</div>';
+        infoHtml = infoHtml + '</button>';
     }
+
     console.log(infoHtml);
     document.getElementById("requestList").innerHTML = infoHtml;
-  
 }
 
 
