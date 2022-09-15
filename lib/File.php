bf62dd74993b3cd348c23d20963cb45d11ba942f
okdshinya <1206338@st.anan-nct.ac.jp>
2022-09-15 16:34:03 +0000

    写真保存＋表示可能

--- lib/File.php
+++ lib/File.php
@@ -6,19 +6,21 @@
         }
 
         function uploadImage($img){
+
             $path;
+            $file_name = time().'.'.pathinfo($img['name'],PATHINFO_EXTENSION);
             
             $uploaddir = '/var/www/html/uploads/';
-            $upload = $uploaddir.basename($img['name']);
+            $upload = $uploaddir.$file_name;
 
             $pathdir = '/uploads/';
-            $path = $pathdir.basename($img['name']);
+            $path = $pathdir.$file_name;
 
             //写真ファイルか判断してエラーを出したい
 
             move_uploaded_file($img['tmp_name'],$upload);
 
-            return $upload;
+            return $path;
         }
     }
 ?>
