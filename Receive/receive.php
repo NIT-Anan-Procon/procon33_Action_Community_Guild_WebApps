26f6a73e0e0bf92288efe7f0603e0512cdfa60b9
ShoeiAzekawa <1206079@st.anan-nct.ac.jp>
2022-09-21 16:29:10 +0900

    continue

--- Receive/receive.php
+++ Receive/receive.php
@@ -52,7 +52,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src = "../scripts/getRequest.js" type="text/javascript" charset="shift_jis"></script>
-    <script src = "../scripts/scrollMovie.js" type="text/javascript" charset="shift_jis"></script>
+    <script src = "../scripts/scrollMovie.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../stylesheets/receive.css">
     <title>receive page</title>
