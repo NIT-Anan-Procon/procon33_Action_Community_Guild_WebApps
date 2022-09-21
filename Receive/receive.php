5af3b3869b61c246fbf8a35b564f4c4e24e55242
ShoeiAzekawa <1206079@st.anan-nct.ac.jp>
2022-09-21 17:17:30 +0900

    訂正

--- Receive/receive.php
+++ Receive/receive.php
@@ -53,8 +53,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src = "../scripts/getRequest.js" type="text/javascript" charset="shift_jis"></script>
     <script src = "../scripts/scrollMovie.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="../stylesheets/receive.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>    <link rel="stylesheet" type="text/css" href="../stylesheets/receive.css">
     <title>receive page</title>
 </head>
 <body onload = "getReceive();">
