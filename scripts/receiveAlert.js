06bd64a7fc8245120976453a036eeb594c6c3fec
ShoeiAzekawa <1206079@st.anan-nct.ac.jp>
2022-09-22 14:07:25 +0900

    受注画面にSweetAlertの実装

--- scripts/receiveAlert.js
+++ scripts/receiveAlert.js
@@ -3,6 +3,17 @@ $(function() {
         var file = e.target.files[0];
         var reader = new FileReader();
 
+        const maxFileSize = 19980155;
+        if(maxFileSize < file.size) {
+            Swal.fire({
+                icon: 'error',
+                text: '動画ファイルが大きすぎます',
+                showConfirmButton: false,
+                timer: 1500
+            });
+            return false;
+        }
+
         var ext = thie.value.match(/\.(.+)$/)[1];
         switch(ext) {
             case 'mp4':
@@ -28,18 +39,7 @@ $(function() {
                 this.value = "";
         }
 
-        reader.onload = (function(file){
-            return function(e){
-                var url = e.target.result
-                Swal.fire({
-                    imageUrl: url,
-                    imageWidth: 200,
-                    text: "素晴らしい画像ですねアリ！",
-                    showConfirmButton: false,
-                    timer: 1700
-                });
-            }
-        })(file);
+        
 
         reader.readAsDataURL(file);
     });
