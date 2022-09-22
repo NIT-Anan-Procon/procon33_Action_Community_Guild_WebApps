902f96745499e55834b411f85d3327b5b622697c
ShoeiAzekawa <1206079@st.anan-nct.ac.jp>
2022-09-22 13:31:02 +0900

    依頼画面にSweetAlertの実装

--- Request/request.php
+++ Request/request.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://unpkg.com/sweetalert2@11.4.33/dist/sweetalert2.all.js"></script>
-    <script src = "../scripts/confirmAlert"></script>
+    <script src = "../scripts/confirmAlert.js"></script>
     <link rel="stylesheet" href="../stylesheets/request.css">
     <title>REQUEST SCREEN</title>
 </head>
