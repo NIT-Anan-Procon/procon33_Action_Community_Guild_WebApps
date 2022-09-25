7a9ebb7ebf30c46eafc68c454a836f7b2892c683
ShoeiAzekawa <1206079@st.anan-nct.ac.jp>
2022-09-25 13:20:20 +0900

    スクロール改善３

--- scripts/scrollMovie.js
+++ scripts/scrollMovie.js
@@ -4,5 +4,49 @@ $(function() {
         $("#forth").scrollTop(400);
         $("#p"+1).get(0).play();
     });
-    
+    $("#p" + 1).on('ended',function() {
+        $("#forth").scrollTop(0);
+        $("#forth").scrollTop(800);
+        $("#p"+2).get(0).play();
+    });
+    $("#p" + 2).on('ended',function() {
+        $("#forth").scrollTop(0);
+        $("#forth").scrollTop(400);
+        $("#p"+3).get(0).play();
+    });
+    $("#p" + 3).on('ended',function() {
+        $("#forth").scrollTop(0);
+        $("#forth").scrollTop(400);
+        $("#p"+4).get(0).play();
+    });
+    $("#p" + 4).on('ended',function() {
+        $("#forth").scrollTop(0);
+        $("#forth").scrollTop(400);
+        $("#p"+5).get(0).play();
+    });
+    $("#p" + 5).on('ended',function() {
+        $("#forth").scrollTop(0);
+        $("#forth").scrollTop(400);
+        $("#p"+6).get(0).play();
+    });
+    $("#p" + 6).on('ended',function() {
+        $("#forth").scrollTop(0);
+        $("#forth").scrollTop(400);
+        $("#p"+7).get(0).play();
+    });
+    $("#p" + 7).on('ended',function() {
+        $("#forth").scrollTop(0);
+        $("#forth").scrollTop(400);
+        $("#p"+8).get(0).play();
+    });
+    $("#p" + 8).on('ended',function() {
+        $("#forth").scrollTop(0);
+        $("#forth").scrollTop(400);
+        $("#p"+9).get(0).play();
+    });
+    $("#p" + 9).on('ended',function() {
+        $("#forth").scrollTop(0);
+        $("#forth").scrollTop(400);
+        $("#p"+10).get(0).play();
+    });
 });
