92dc934bf8bea83edf24a631c58273f4a9c84af2
ShoeiAzekawa <1206079@st.anan-nct.ac.jp>
2022-10-01 14:40:16 +0900

    受注画面のバグ修正

--- Receive/receive.php
+++ Receive/receive.php
@@ -98,7 +98,7 @@
                         $movie = $value[0];
                         ?>
                             <div class="movie" id="movie">
-                                <video id="p<?php echo $count?>" controls poster="../images/poster_passione.png" preload="none" onclick="this.play();" src="<?php echo $movie;?>">
+                                <video id="p<?php echo $count?>" controls poster="../images/poster_passione.png" preload="none" playsinline>
                                     <source src="<?php echo $movie;?>">
                                 </video>
                             </div>
